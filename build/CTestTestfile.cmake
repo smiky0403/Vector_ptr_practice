0663312679a6b6c71369a84bd4648a593fda6119
Mike <smingqi@gmail.com>
2022-03-20 14:42:21 -0400

    Delete Build Director and a.out

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mingqis0403/Vector_ptr_practice
-# Build directory: /home/mingqis0403/Vector_ptr_practice/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
